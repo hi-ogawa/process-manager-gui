f7759243e435e6a22379207f52ed76ca10a52457
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-09-17 19:31:16 +0900

    chore: mockup process manager

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,18 +1,25 @@
 import path from "path";
 import { BrowserWindow, app, ipcMain } from "electron";
 
+const PRELOAD_SCRIPT = path.resolve(__dirname, "index-preload.cjs");
+
+const RENDERER_URL =
+  process.env["APP_RENDERER_URL"] ??
+  new URL(`file://${__dirname}/../../renderer/build/index.html`).toString();
+
 async function main() {
   await app.whenReady();
+  setupIpc();
   const window = new BrowserWindow({
     webPreferences: {
-      preload: path.resolve(__dirname, "index-preload.cjs"),
+      preload: PRELOAD_SCRIPT,
     },
   });
+  await window.loadURL(RENDERER_URL);
+}
+
+function setupIpc() {
   ipcMain.handle("ping", () => "pong");
-  const url =
-    process.env["APP_RENDERER_URL"] ??
-    new URL(`file://${__dirname}/../../renderer/build/index.html`).toString();
-  await window.loadURL(url);
 }
 
 if (require.main === module) {
