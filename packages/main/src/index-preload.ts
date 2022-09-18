6736ff0030230603491c7da10369d33197a66e92
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-09-18 15:48:06 +0900

    feat: prototype config ipc + command list ui

--- packages/main/src/index-preload.ts
+++ packages/main/src/index-preload.ts
@@ -1,12 +1,47 @@
 import { contextBridge, ipcRenderer } from "electron";
 
-// TODO: typing
 function main() {
-  contextBridge.exposeInMainWorld("preloadApi", {
-    ping: () => {
-      return ipcRenderer.invoke("ping");
-    },
+  contextBridge.exposeInMainWorld("PRELOAD_API", {
+    "/config/get": (...args: any[]) =>
+      ipcRenderer.invoke("/config/get", ...args),
+    "/config/update": (...args: any[]) =>
+      ipcRenderer.invoke("/config/update", ...args),
   });
 }
+
+// type of IPC
+// - r2m2r: renderer --(invoke)--> main --(handle)--> renderer
+// - m2r: main --(sebContents.send)--> renderer
+
+// interface IpcSpecR2M2R {
+//   "/config/get": undefined;
+//   "/config/update": undefined;
+//   "/process/start": undefined;
+//   "/process/stop": undefined;
+//   "/status/get": undefined;
+// }
+
+// interface IpcSpecM2R {
+//   "/change": undefined;
+// }
+
+//
+// [interface]
+//
+// in renderer
+//
+//   const res = await r2m2r.invoke("/config/get", req);
+//   const res = await r2m2r.invoke("/config/update", req);
+//   const res = await r2m2r.invoke("/status/get", req);
+//   const res = await r2m2r.invoke("/log/get", req);
+//
+//   m2r.on("/changed", (event) => {});
+//
+
+//
+// in main
+//
+//   r2m2r.handle("/config/get", () => {})
+//
 
 main();
--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,5 +1,9 @@
+import fs from "fs";
 import path from "path";
-import { BrowserWindow, app, ipcMain } from "electron";
+import { BrowserWindow, Menu, app, ipcMain } from "electron";
+
+// TODO: open config file directly in editor from menu?
+const CONFIG_PATH = path.resolve(app.getPath("userData"), "config.json");
 
 const PRELOAD_SCRIPT = path.resolve(__dirname, "index-preload.cjs");
 
@@ -9,17 +13,57 @@ const RENDERER_URL =
 
 async function main() {
   await app.whenReady();
+  await ensureConfig();
   setupIpc();
+  await createWindow();
+}
+
+function setupIpc() {
+  ipcMain.handle("/config/get", async () => {
+    const config = await fs.promises.readFile(CONFIG_PATH, "utf-8");
+    return JSON.parse(config);
+  });
+
+  ipcMain.handle("/config/update", async (_event, config: any) => {
+    await fs.promises.writeFile(CONFIG_PATH, JSON.stringify(config, null, 2));
+  });
+}
+
+async function ensureConfig() {
+  if (!fs.existsSync(CONFIG_PATH)) {
+    await fs.promises.writeFile(CONFIG_PATH, "{}");
+  }
+}
+
+async function createWindow() {
   const window = new BrowserWindow({
     webPreferences: {
       preload: PRELOAD_SCRIPT,
     },
   });
+  window.webContents.once("dom-ready", () => {
+    createContextMenu(window);
+  });
   await window.loadURL(RENDERER_URL);
 }
 
-function setupIpc() {
-  ipcMain.handle("ping", () => "pong");
+function createContextMenu(window: BrowserWindow) {
+  window.webContents.on("context-menu", (_event, props) => {
+    const menu = Menu.buildFromTemplate([
+      // https://github.com/electron/fiddle/blob/19360ade76354240630e5660469b082128e1e57e/src/main/context-menu.ts#L113
+      {
+        id: "inspect",
+        label: "Inspect",
+        click: () => {
+          window.webContents.inspectElement(props.x, props.y);
+          if (!window.webContents.isDevToolsOpened()) {
+            window.webContents.devToolsWebContents?.focus();
+          }
+        },
+      },
+    ]);
+    menu.popup({});
+  });
 }
 
 if (require.main === module) {
